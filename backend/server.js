51e14eec98a7401fd6a285c8a9d221e55ba17e03
kumar ravikar <kumarravikar24@gmail.com>
2026-07-09 01:30:16 +0530

    resolving port error

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import  app  from "./src/app.js";
 import connectDB from "./src/db/db.js";
 
+
+const PORT = process.env.PORT || 3000
  connectDB()
-app.listen(process.env.PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`server is listening on port ${process.env.PORT}`)
 })
