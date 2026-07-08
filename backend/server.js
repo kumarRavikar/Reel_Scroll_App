092a0067372de7f36a8c9483673d409bd80e2fdb
kumar ravikar <kumarravikar24@gmail.com>
2026-07-09 01:49:25 +0530

    remove port from .env

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import  app  from "./src/app.js";
 import connectDB from "./src/db/db.js";
 
 
-
+const PORT = process.env.PORT || 3000
  connectDB()
-app.listen(process.env.PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`server is listening on port ${process.env.PORT}`)
 })
