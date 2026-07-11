937649bd116710b443765f053d36b593e8c0f612
kumar ravikar <kumarravikar24@gmail.com>
2026-07-11 17:30:33 +0530

    fixing error given by cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import cors from "cors";
 dotenv.config()
 const app = express()
 app.use(cors({
-    origin:"https://reel-scroll-jtrryz8tu-kumar-ravikars-projects.vercel.app",
+    origin:"https://reel-scroll-app.vercel.app",
     credentials:true
 }))
 
